f9b2c9ca12ee72692b74ddcfac1c59ac344e4eed
tsung <tsung@localhost.localdomain>
2019-10-07 11:56:15 +0800

    bug fix

--- draw.js
+++ draw.js
@@ -1,17 +1,3 @@
-
-// var d3 = require('d3');
-// var jsdom = require("jsdom");
-// const { JSDOM } = jsdom;
-// const { window } = new JSDOM();
-// const { document } = (new JSDOM('')).window;
-// global.document = document;
-//
-// var $ = jQuery = require('jquery')(window);
-//
-// var $ = jQuery = require('jquery')(window);
-// $("body").append("<div id = pedigrees></div>");
-
-
 const { JSDOM } = require('jsdom');
 
 const options = {
@@ -23,8 +9,4 @@ const options = {
 JSDOM.fromFile('example1.html', options).then((dom) => {
     const { document } = dom.window;
     var root = document.querySelector("body");
-    // console.log(root.innerHTML);
-    // console.log(dom.window.querySelector("p"))
-    //
-    // console.log(document.getElementById("#pedigrees").innerHTML);
 });
--- pedigree.js
+++ pedigree.js
@@ -1535,6 +1535,7 @@
 						return y_offset;
 					},
 					function(d) {
+
 						var dis = disease.replace('_', ' ').replace('cancer', 'ca.');
 						return disease+'_diagnosis_age' in d.data ? dis +": "+ d.data[disease+'_diagnosis_age'] : '';
 					}, 'indi_details');
@@ -3638,48 +3639,8 @@
 }(window.widgets = window.widgets || {}, jQuery));
 
 //# sourceMappingURL=pedigreejs.js.map
-// function testing()
-// {
-// 	var parent_width = $('#pedigrees').parent().width();
-// 	var margin = ($(window).width()-parent_width > 10 ? 100 : 30);
-// 	var svg_width = (parent_width > 750 ? (parent_width*8/12 - margin) : parent_width- margin);
-//
-// 	var dataset = [
-// 		{"name": "m11", "display_name": "John",  "sex": "M", "diabetes_diagnosis_age": 55, "top_level": true},
-// 		{"name": "f11", "display_name": "Jane",  "sex": "F", "status": 1, "top_level": true},
-// 		{"name": "m12", "display_name": "Jack", "sex": "M", "top_level": true},
-// 		{"name": "f12", "display_name": "Jill", "sex": "F", "top_level": true},
-// 		{"name": "m21", "display_name": "Jim", "sex": "M", "mother": "f11", "father": "m11", "age": 56},
-// 		{"name": "f21", "display_name": "Jan", "sex": "F", "mother": "f12", "father": "m12", "age": 63},
-// 		{"name": "ch1", "display_name": "Ana", "sex": "F", "mother": "f21", "father": "m21", "proband": true, "age": 25}
-// 	];
-// 	var opts = {
-// 		'targetDiv': 'pedigrees',
-// 		'btn_target': 'diabetes_history',
-// 		'store_type': 'session',
-// 		'width': 600,
-// 		'height': 600,
-// 		'symbol_size': 35,
-// 		'edit': true,
-// 		'diseases': [
-// 			{'type': 'diabetes', 'colour': '#F68F35'},
-// 		],
-// 		'DEBUG': (window.pedigree_util.urlParam('debug') === null ? false : true)
-// 	};
-//
-// 	$('#opts').append(JSON.stringify(opts, null, 4));
-//
-// 	var local_dataset = window.window.pedcache.current(opts);
-// 	if (local_dataset !== undefined && local_dataset !== null) {
-// 		opts.dataset = local_dataset;
-// 	} else {
-// 		opts.dataset = dataset;
-// 	};
-// 	opts= window.ptree.build(opts);
-// 	// console.log(document.getElementById("pedigrees").innerHTML);
-// 	console.log(io.get_printable_svg(opts).html());
-// };
-function testing()
+
+function pedigree()
 {
 	var parent_width = $('#pedigrees').parent().width();
 	var margin = ($(window).width()-parent_width > 10 ? 100 : 30);
@@ -3696,6 +3657,10 @@ function testing()
 			'font_family': 'times',
 			'font_weight': 400,
 			'node_background': '#fff',
+			diseases: [	{'type': 'breast_cancer', 'colour': '#F68F35'},
+						{'type': 'ovarian_cancer', 'colour': '#4DAA4D'},
+						{'type': 'pancreatic_cancer', 'colour': '#4289BA'},
+						{'type': 'prostate_cancer', 'colour': '#D5494A'}],
 			'DEBUG': (pedigree_util.urlParam('debug') === null ? false : true)
 		};
 
